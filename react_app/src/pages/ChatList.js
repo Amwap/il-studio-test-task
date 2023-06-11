cb4ca64af7b87290b4ff25457f29e6af8b21edb3
Ilya <ilyakonovalov12@gmail.com>
2023-06-12 00:08:54 +0400

    comleted chatlist

--- react_app/src/pages/ChatList.js
+++ react_app/src/pages/ChatList.js
@@ -1,16 +1,11 @@
-// import Button from "@material-ui/core/Button";
-// import CssBaseline from "@material-ui/core/CssBaseline";
-// import TextField from "@material-ui/core/TextField";
-// import Container from "@material-ui/core/Container";
-// import Card from "@material-ui/core/Card";
-// import CardHeader from "@material-ui/core/CardHeader";
-// import Paper from "@material-ui/core/Paper";
 import { withStyles } from "@material-ui/core/styles";
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { ArrowRightCircle } from 'react-bootstrap-icons';
 
 const useStyles = (theme) => ({
     submit: {
@@ -22,15 +17,31 @@ class ChatList extends Component {
     render() {
         return (
             <div id="chatlist-layout">
-                <Card className="col-md-4">
+                <Card className="col-md-4 text-nowrap" style={{ minWidth: '320px' }}>
                     <Card.Header style={{ fontSize: "26px", backgroundColor: 'white' }} className="d-flex justify-content-center">Выберите / создайте чат</Card.Header>
                     <Card.Body className="d-flex flex-column align-items-center">
+                        <ListGroup as="ul" className="col-12 mb-3">
+                            <ListGroup.Item
+                                    className="d-flex flex-row justify-content-between align-items-center"
+                                    style={{ backgroundColor: '#7879F1', color: 'white' }}
+                                    action href="#link1"
+                                >
+                                Новая комната <ArrowRightCircle className='ml-auto' style={{ fontSize: 'large'}}/>
+                            </ListGroup.Item>
+                            <ListGroup.Item
+                                    className="d-flex flex-row justify-content-between align-items-center"
+                                    style={{ backgroundColor: '#7879F1', color: 'white' }}
+                                    action href="#link2"
+                                >
+                                Новая комната <ArrowRightCircle className='ml-auto' style={{ fontSize: 'large'}}/>
+                            </ListGroup.Item>
+
+                        </ListGroup>
                         <InputGroup className="mb-3">
-                            <Form.Control placeholder="Логин" />
-                        </InputGroup>
-                        <InputGroup className="mb-3">
-                            <Form.Control placeholder="Пароль" />
-                            <Button variant="primary" style={{ width: '102px', height: '38px' }}>Войти</Button>
+                            <div className="input-with-inner-button d-flex flex-row">
+                                <Form.Control placeholder="Введите название чата" style={{ border: 'white' }} />
+                                <Button variant="primary" style={{ width: '102px', height: '38px' }} className="m-1">Создать</Button>
+                            </div>
                         </InputGroup>
                     </Card.Body>
                 </Card>
