a74d04eaaa3230f340a661cccf0c8abebe3f1bd9
Ilya <ilyakonovalov12@gmail.com>
2023-06-13 03:29:28 +0400

    recreated react app with jwt

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -1,32 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-// Bootstrap CSS
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './index.css';
 
-import Login from "./pages/Login";
-import Chat from "./pages/Chat";
-import Layout from "./pages/Layout";
-import ChatList from './pages/ChatList';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="login" element={<Login />} />
-          <Route path="chatlist" element={<ChatList />} />
-          <Route path="chat" element={<Chat />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- react_app/src/pages/Login.js
+++ react_app/src/pages/Login.js
@@ -1,35 +1,55 @@
-import { withStyles } from "@material-ui/core/styles";
-import React, { Component } from 'react';
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import axios from "axios";
+import { setAuthToken } from "../helpers/setAuthToken"
 
-const useStyles = (theme) => ({
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
-});
+function Login() {
+    const handleSubmit = (email, password) => {
+        //reqres registered sample user
+        const loginPayload = {
+            email: 'eve.holt@reqres.in',
+            password: 'cityslicka'
+        }
 
-class Login extends Component {
-    render() {
-        return (
-            <div id="login-layout">
-                <Card className="col-md-4">
-                    <Card.Header style={{ fontSize: "26px", backgroundColor: 'white' }} className="d-flex justify-content-center">Авторизация</Card.Header>
-                    <Card.Body className="d-flex flex-column align-items-center">
-                        <InputGroup className="mb-3">
-                            <Form.Control placeholder="Логин" />
-                        </InputGroup>
-                        <InputGroup className="mb-3">
-                            <Form.Control placeholder="Пароль" />
-                        </InputGroup>
-                        <Button variant="primary" style={{ width: '102px', height: '38px' }}>Войти</Button>
-                    </Card.Body>
-                </Card>
-            </div>
-        )
+        axios.post(`${process.env.REACT_APP_API_URL}/api/v1/token/`, loginPayload)
+            .then(response => {
+                const token = response.data.token;
+                localStorage.setItem("token", token);
+                setAuthToken(token);
+                window.location.href = '/'
+            })
+            .catch(err => console.log(err));
     }
+    return (
+        <div id="login-layout">
+            <Card className="col-md-4 col-lg-3">
+                <Card.Header style={{ fontSize: "26px", backgroundColor: 'white' }} className="d-flex justify-content-center">Авторизация</Card.Header>
+                <Card.Body className="d-flex flex-column align-items-center">
+                    <form 
+                        className='col-12'
+                        onSubmit={(event) => {
+                            event.preventDefault()
+                            const [email, password] = event.target.children;
+                            handleSubmit(email, password);
+                        }}
+                    >
+
+                        <InputGroup className="mb-3">
+                            <Form.Control placeholder="Логин" name="email"/>
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                            <Form.Control placeholder="Пароль" name="password" type='password'/>
+                        </InputGroup>
+                        <Button type="submit" variant="primary" style={{ width: '102px', height: '38px' }}>Войти</Button>
+                    </form>
+                </Card.Body>
+            </Card>
+        </div>
+    )
 }
 
-export default withStyles(useStyles)(Login);
+export default Login;
+
